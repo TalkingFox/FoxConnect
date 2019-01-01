4c23e661f8fe787f3b4dc4aa90bd48e2ad73bd6e
Mike Wilkinson <mwilkinson@imo-online.com>
2019-01-01 16:32:56 -0600

    add peer logic to client

--- lib/index.ts
+++ lib/index.ts
@@ -1,10 +1,12 @@
 import { FoxConnectOptions } from "./models/foxConnectOptions";
 import { IotClient, ListenCallback } from "./iot/iotClient";
-import { HttpUtils } from "./http";
+import { HttpUtils } from "./util/http";
 import { AcceptGuestRequest } from "./models/acceptGuestRequest";
 import { JoinRoomRequest } from "./models/joinRoomRequest";
 import { HostResponse } from "./models/hostResponse";
 import { JoinRoomResponse } from "./models/joinRoomResponse";
+import Peer, { Instance } from 'simple-peer';
+import { TimedPromise } from "./util/timedPromise";
 
 export class Host {
     private iot: IotClient;
@@ -42,16 +44,30 @@ export class Host {
     }
 }
 
+export type MessageReceivedCallback = (message: string) => void;
+
 export class Client {
     private iot: IotClient;
+    private peer: Instance;
+    private onMessageReceived: MessageReceivedCallback;
 
     constructor(private options: FoxConnectOptions) {
         this.iot = new IotClient(options);
     }
 
-    public joinRoom(request: JoinRoomRequest): Promise<HostResponse> {
-        const promise = new Promise<HostResponse>((resolve, reject) => {
-            const endpoint = `${this.options.signalServer}/rooms/${request.room}/join`;
+    public listenForMessages(messageCallback: MessageReceivedCallback): void {
+        this.onMessageReceived = messageCallback;
+    }
+
+    public joinRoom(room: string, playerName: string = Math.floor(Math.random() * 1000000 + 1).toString()): Promise<void> {
+        this.peer = new Peer({ initiator: true, trickle: false });
+        this.peer.on('signal', (id: any) => {
+            const request: JoinRoomRequest = {
+                offer: JSON.stringify(id),
+                player: playerName,
+                room: room
+            };
+            const endpoint = `${this.options.signalServer}/rooms/${room}/join`;
             HttpUtils.post<JoinRoomResponse>(
                 endpoint,
                 {
@@ -61,8 +77,17 @@ export class Client {
                 .then((response: JoinRoomResponse) =>
                     this.iot.subscribe(
                         response.roomTopic,
-                        (response: HostResponse) => resolve(response))
+                        (response: HostResponse) => {
+                            this.peer.signal(response.answer);
+                        })
                 );
+        });
+        
+        const promise = TimedPromise<void>(30 * 1000, (resolve, reject) => {
+            this.peer.on('connect', () => {
+                this.peer.on('data', this.onMessageReceived);
+                resolve();
+            });
         });
         return promise;
     }
